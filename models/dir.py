216bb06728ac771c21e74f9ee3f62f7ff4cc3c93
DanishAli01 <danishali2158@gmail.com>
2019-04-03 15:28:50 +0100

    Folder Model Added

---
+++ models/dir.py
@@ -0,0 +1,11 @@
+from google.appengine.ext import ndb
+
+
+class Folder(ndb.Model):
+    # name & path
+    name, path = ndb.StringProperty(), ndb.StringProperty()
+
+    # The key of the directory to navigate and interact with root
+    # all sub directories of given directory
+    # keys of all the files in given directory
+    root_dir, dirs, files = ndb.KeyProperty(), ndb.KeyProperty(repeated=True),ndb.KeyProperty(repeated=True)
